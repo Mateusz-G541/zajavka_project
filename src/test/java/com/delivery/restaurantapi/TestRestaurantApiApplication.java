709e4da389b33ce07e244da07ecf4245b7c78b38
mateusz <mateuszgolen1994@gmail.com>
2023-10-18 12:55:17 +0200

    ℹ️ Initialize Project with Entity Classes

---
+++ src/test/java/com/delivery/restaurantapi/TestRestaurantApiApplication.java
@@ -0,0 +1,23 @@
+package com.delivery.restaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestRestaurantApiApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(RestaurantApiApplication::main).with(TestRestaurantApiApplication.class).run(args);
+    }
+
+}
